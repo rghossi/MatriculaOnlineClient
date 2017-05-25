059384223460988c171df23494ba48bd6b32c58b
Dioginis <dioginiscarvalho@hotmail.com>
2017-05-24 21:39:27 -0300

    add noticias na tela inicial

--- src/components/TelaInicial.js
+++ src/components/TelaInicial.js
@@ -9,11 +9,11 @@ class TelaInicial extends Component {
             <div className="row">
                 <div className="row">
 
-                  <div className="col s12 m4">
+                <div className="col s12 m4">
                     <div className="card  indigo darken-4">
                       <div className="card-content white-text">
-                        <span className="card-title cyan-text accent-1"><b>Card Title</b></span>
-                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        <span className="card-title cyan-text accent-1"><b>IGC: UENF repete a melhor pontuação do RJ</b></span>
+                        <p>Pelo segundo ano consecutivo, a UENF obteve a melhor pontuação entre todas as universidades do Rio de Janeiro no Índice Geral de Cursos (IGC) do MEC. A Universidade figura com o 12º melhor índice entre as universidades de todo o país no IGC 2012. Desde que o IGC foi criado, são cinco anos em que a UENF permanece entre as 15 universidades de melhor avaliação no país: 2008, 2009, 2010, 2011 e 2012.</p>
                       </div>
                     </div>
                   </div>
@@ -21,8 +21,8 @@ class TelaInicial extends Component {
                   <div className="col s12 m4">
                     <div className="card  indigo darken-4">
                       <div className="card-content white-text">
-                        <span className="card-title cyan-text accent-1"><b>Card Title</b></span>
-                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        <span className="card-title cyan-text accent-1"><b>II Congresso Fluminense de Pós-Graduação</b></span>
+                        <p>Estão abertas as inscrições para o II Congresso Fluminense de Pós-Graduação, realizado pela UENF, em parceria com a UFF e o IFF (campus Centro). O evento será simultâneo com o IX Congresso Fluminense de Iniciação Científica e Tecnológica (CONFICT), que tem como tema neste ano: “Ciência, tecnologia e inovação no Brasil – desafios e transformações”. Os interessados devem se inscrever através do endereço http://www.iiconpg.iff.edu.br/. </p>
                       </div>
                     </div>
                   </div>
@@ -30,8 +30,8 @@ class TelaInicial extends Component {
                   <div className="col s12 m4">
                     <div className="card  indigo darken-4">
                       <div className="card-content white-text">
-                        <span className="card-title cyan-text accent-1"><b>Card Title</b></span>
-                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        <span className="card-title cyan-text accent-1"><b>Bolsas de Iniciação Científica</b></span>
+                        <p>Estarão abertas, no período de 24 de maio a 11 de junho de 2017, as inscrições para o Edital de Bolsas de Iniciação Científica 2017-2. Serão oferecidas 32 vagas para início das bolsas em agosto de 2017. Estudantes voluntários inseridos em projetos de pesquisa também podem se inscrever para participar do Programa Institucional de Iniciação Científica e Tecnológica. Os critérios e requisitos necessários para concorrer estão disponíveis e você encontra todas as informações do <a href="http://uenf.br/projetos/pibic/files/2017/05/EDITAL-PIBIC-2017-02-1.pdf&;source=gmail&ust=1495732999339000&usg=AFQjCNF-ogzjODKk2DSTNS4aL-mlwsq6IA%22%20style=%22color:%20rgb(17,%2085,%20204">Edital</a>.</p>
                       </div>
                     </div>
                   </div>
@@ -39,8 +39,8 @@ class TelaInicial extends Component {
                   <div className="col s12 m4">
                     <div className="card  indigo darken-4">
                       <div className="card-content white-text">
-                        <span className="card-title cyan-text accent-1"><b>Card Title</b></span>
-                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        <span className="card-title cyan-text accent-1"><b>Concurso para professor associado e titular</b></span>
+                        <p>Estão abertas as inscrições para quatro vagas de professor da UENF. Uma das vagas é para professor titular do Laboratório de Ciências Físicas (LCFIS) do Centro de Ciência e Tecnologia (CCT). As outras três vagas são para professor associado, sendo uma também para o LCFIS e duas para o Laboratório de Gestão e Políticas Públicas (LGPP) do Centro de Ciências do Homem (CCH).</p>
                       </div>
                     </div>
                   </div>
@@ -48,8 +48,8 @@ class TelaInicial extends Component {
                   <div className="col s12 m4">
                     <div className="card  indigo darken-4">
                       <div className="card-content white-text">
-                        <span className="card-title cyan-text accent-1"><b>Card Title</b></span>
-                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        <span className="card-title cyan-text accent-1"><b>Esclarecimento a Comunidade</b></span>
+                        <p>Causou-nos profunda indignação o ato publicado às páginas 22 do Diário Oficial do Estado do Rio de Janeiro do dia 22/05/2017, que trata do cancelamento do concurso público para professores da UENF, publicado no DOE-RJ em 27 de abril último. A iniciativa de publicar o ato de cancelamento do edital, embora imputada ao Reitor, não partiu desta reitoria nem de nenhum setor desta Universidade. Não solicitamos nem autorizamos o cancelamento do concurso em andamento.</p>
                       </div>
                     </div>
                   </div>
@@ -57,8 +57,8 @@ class TelaInicial extends Component {
                   <div className="col s12 m4">
                     <div className="card  indigo darken-4">
                       <div className="card-content white-text">
-                        <span className="card-title cyan-text accent-1"><b>Card Title</b></span>
-                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                        <span className="card-title cyan-text accent-1"><b>Agência UENF de Inovação</b></span>
+                        <p>A Agência UENF de Inovação, a AgiUENF, foi instituída pela resolução n.5/2011 do Conselho Universitário (CONSUNI) da UENF, de 4 de novembro de 2011. São competências da Agência UENF de Inovação: Atuar como proponente e gestora da política de inovação da UENF, conforme Lei Estadual Nº 5.361 de 29 de dezembro de 2008. Atuar no desenvolvimento das relações Universidade – Empresa, inclusive com as fundações de apoio.</p>
                       </div>
                     </div>
                   </div>
