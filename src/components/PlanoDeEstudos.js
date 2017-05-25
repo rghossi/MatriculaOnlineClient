7b5cb65d752a66a57b7cec52bdec5673344993a0
Gabriel Fiorese <Gabriel Fiorese>
2017-05-24 21:43:14 -0300

    adicionei o plano de estudos

--- src/components/PlanoDeEstudos.js
+++ src/components/PlanoDeEstudos.js
@@ -1,49 +1,278 @@
 import React, { Component } from 'react';
-// import grade1 from './imagens/grade1.png';
 
 class PlanoDeEstudos extends Component {
   render() {
     return (
-      <div>
-        <h3 className="center indigo-text darken-4">Plano de Estudos</h3>
-        <div className="row">
-          <div className="col s12 m1 indigo-text darken-4"></div>
-          <div className="col s12 m2 indigo-text darken-4">SEGUNDA</div>
-          <div className="col s12 m2 indigo-text darken-4">TERÇA</div>
-          <div className="col s12 m2 indigo-text darken-4">QUARTA</div>
-          <div className="col s12 m2 indigo-text darken-4">QUINTA</div>
-          <div className="col s12 m2 indigo-text darken-4">SEXTA</div>
+      
+      <div id="card-panel-type" className="section">
+            <h3 className="center indigo-text darken-4">Plano de Estudos</h3>
+            <div className="row">
+                <div className="row">
+
+                  <div className="col s12 m1"></div>
+
+                  <div className="col s12 m2">
+                    <div className="card  indigo darken-4">
+                      <div className="card-content center-align">
+                        <span className="card-title amber-text text-lighten-2"><b>Segunda</b></span>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>08:00 - 10:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>10:00 - 12:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p>12:00 - 14:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>14:00 - 16:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>16:00 - 18:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>18:00 - 20:00</p>
+                            </div>
+                          </div>
+
+
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className="col s12 m2">
+                    <div className="card  indigo darken-4">
+                      <div className="card-content center-align">
+                        <span className="card-title amber-text text-lighten-2"><b>Terça</b></span>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Calculo I</b></p>
+                              <p>08:00 - 10:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Fisica I</b></p>
+                              <p>10:00 - 12:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p>12:00 - 14:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>14:00 - 16:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>16:00 - 18:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>18:00 - 20:00</p>
+                            </div>
+                          </div>
+
+
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className="col s12 m2">
+                    <div className="card  indigo darken-4">
+                      <div className="card-content center-align">
+                        <span className="card-title amber-text text-lighten-2"><b>Quarta</b></span>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>08:00 - 10:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>10:00 - 12:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p>12:00 - 14:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>14:00 - 16:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>16:00 - 18:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>18:00 - 20:00</p>
+                            </div>
+                          </div>
+
+
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className="col s12 m2">
+                    <div className="card  indigo darken-4">
+                      <div className="card-content center-align">
+                        <span className="card-title amber-text text-lighten-2"><b>Quinta</b></span>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Calculo I</b></p>
+                              <p>08:00 - 10:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Fisica I</b></p>
+                              <p>10:00 - 12:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p>12:00 - 14:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>14:00 - 16:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>16:00 - 18:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Programação I</b></p>
+                              <p>18:00 - 20:00</p>
+                            </div>
+                          </div>
+
+
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <div className="col s12 m2">
+                    <div className="card  indigo darken-4">
+                      <div className="card-content center-align">
+                        <span className="card-title amber-text text-lighten-2"><b>Sexta</b></span>
+
+                          <div className="card cyan">
+                            <div className="card-content indigo-text text-darken-4 center-align tooltipped" data-delay="50" data-tooltip="Sala 109 - CCT">
+                              <p><b>Calculo I</b></p>
+                              <p>08:00 - 10:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>10:00 - 12:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p>12:00 - 14:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>14:00 - 16:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>16:00 - 18:00</p>
+                            </div>
+                          </div>
+
+                          <div className="card indigo darken-4">
+                            <div className="card-content white-text center-align">
+                              <p><br/></p>
+                              <p>18:00 - 20:00</p>
+                            </div>
+                          </div>
+
+
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+            </div>
         </div>
-        <div className="divider"></div>
-
-        <div className="row">
-            <div className="card-panel teal" height="100" width="100">
-              <span className="white-text">Programação 1</span>
-          </div>
-
-          <div className="col s3">
-            <div className="card-panel teal">
-              <span className="white-text">Programação 1</span>
-            </div>
-          </div>
-          <div className="col s3">
-            <div className="card-panel teal">
-              <span className="white-text">Programação 1</span>
-            </div>
-          </div>
-          <div className="col s3">
-            <div className="card-panel teal">
-              <span className="white-text">Programação 1</span>
-            </div>
-          </div>
-          <div className="col s3">
-            <div className="card-panel teal">
-              <span className="white-text">Programação 1</span>
-            </div>
-          </div>
-        </div>
-
-      </div>
     );
   }
 }
